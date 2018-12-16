6cd3dcb4c62e5bd6f41160a2be779683af47bafe
yethuaungptu <yethuaung481995@gamil.com>
2018-12-16 13:59:06 +0630

    assign project path

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var student = require('./routes/admin/student');
+var teacher = require('./routes/admin/teacher');
+var subject = require('./routes/admin/subject');
 
 var app = express();
+
+mongoose.connect('mongodb://127.0.0.1/sasdb')
+// mongoose.connect('mongodb://yethuaung:Zikimi95@ds125048.mlab.com:25048/studynode');
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/students', student);
+app.use('/admin/teachers', teacher);
+app.use('/admin/subjects', subject);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

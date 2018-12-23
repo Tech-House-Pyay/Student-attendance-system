fcd1b3f65652c998818d9a6cb07a709346db8d7e
wintwah001 <wintwahh399@gmail.com>
2018-12-23 09:57:52 +0700

    subject CRUD

---
+++ model/Subject.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SubSchema=new Schema({
+  teacher_id:{
+    type:Schema.Types.ObjectId,
+    ref:'Teachers',
+  },
+  subname:{
+    type:String,
+    require:true,
+  },
+  major:{
+    type:String,
+    require:true,
+  },
+  year:{
+    type:String,
+    require:true,
+  },
+});
+module.exports=mongoose.model('Subject',SubSchema);
--- routes/admin/subject.js
+++ routes/admin/subject.js
@@ -1,9 +1,70 @@
 var express = require('express');
 var router = express.Router();
-
+var Subject= require('../../model/Subject');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/add',function(req,res,next){
+  res.render('admin/subject/subj-add');
+});
+router.post('/add',function(req,res,next){
+  console.log('call');
+  var subject=new Subject();
+  // subject.teacher_id=req.body.teacherid;
+  subject.subname=req.body.name;
+  subject.major=req.body.major;
+  subject.year=req.body.year;
+  subject.save(function(err,rtn){
+    if (err)throw err;
+    res.redirect('/admin/subjects/subject-detail/'+rtn._id);
+  });
 
+});
+router.post('/update',function(req,res,next){
+  console.log('call');
+  var update={
+    name:req.body.subname,
+    major:req.body.major,
+    year:req.body.year,
+  }
+    Subject.findByIdAndUpdate(req.body.id,{$set:update},function(err,rtn){
+      if(err) throw err;
+      console.log(rtn);
+      res.redirect('/admin/subjects/subject-detail/'+rtn._id);
+    });
+
+});
+router.get('/update/:id',function(req,res,next){
+  console.log('call');
+  Subject.findById(req.params.id,function (err,rtn) {
+    if(err)throw err;
+    console.log(rtn);
+    res.render('admin/subject/subject-update',{subject:rtn});
+
+  });
+});
+router.get('/subject-detail/:id',function(req,res,next){
+  Subject.findById(req.params.id,function(err,rtn){
+    if (err) throw err;
+    console.log(rtn);
+    res.render('admin/subject/subject-detail',{subject:rtn});
+  });
+});
+router.get('/list',function(req,res,next){
+  Subject.find({},function(err,rtn){
+    if(err) throw err;
+    console.log(rtn);
+    res.render('admin/subject/subject-list',{subject:rtn});
+  });
+});
+router.get('/delete/:id',function (req,res,next) {
+  Subject.findByIdAndRemove(req.params.id,function (err,rtn) {
+    if(err) throw err;
+    console.log(rtn);
+    res.redirect('/admin/subjects/list/');
+
+  });
+
+});
 module.exports = router;

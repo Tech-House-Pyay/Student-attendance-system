ebfffb3c58fe3c79b13bd8254186436d8174c691
khinmamawine <winelay001br@gmail.com>
2019-01-17 17:59:43 +0630

    Merge branch 'master' of https://github.com/Tech-House-Pyay/Student-attendance-system

--- routes/admin/attendance.js
+++ routes/admin/attendance.js
@@ -8,6 +8,21 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/attendancelist',function(req,res,next){
+Attendance.find({}, function(err,rtn){
+    if(err) throw err;
+    console.log(rtn);
+  res.render('admin/attendance/atten-studentdetail');
+});
+});
+
+router.get('/studentdetail',function(req,res,next){
+Attendance.find({}, function(err,rtn){
+    if(err) throw err;
+    console.log(rtn);
+  res.render('admin/attendance/atten-listdetail');
+});
+});
 
   router.get('/atten-detail/:id',function(req,res,next){
   Attendace.findById(req.params.id,function(err,rtn){
@@ -15,6 +30,13 @@ router.get('/', function(req, res, next) {
       console.log(rtn);
         res.render('admin/users/attendance/atten-detail');
   });
+  });
+  router.get('/add',function(req,res,next){
+    res.render('admin/attendance/atten-add');
+  });
+  router.post('/add',function (req,res,next) {
+    res.redirect('/admin/attendance/add');
+
   });
 
 module.exports = router;

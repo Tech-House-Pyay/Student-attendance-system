e42abc9e726a160e2f63dfeb232ddfb586a74051
yethuaungptu <yethuaung481995@gamil.com>
2019-01-19 20:24:52 +0630

    add student...

--- routes/users/student.js
+++ routes/users/student.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  res.render('user/student/home', { title: 'Express' });
+});
+router.get('/timeTab', function(req, res, next) {
+  res.render('user/student/time-table', { title: 'Express' });
+});
+router.get('/attendance', function(req, res, next) {
+  res.render('user/student/attendance', { title: 'Express' });
+});
+router.get('/calculator', function(req, res, next) {
+  res.render('user/student/calculator', { title: 'Express' });
 });
 
 module.exports = router;

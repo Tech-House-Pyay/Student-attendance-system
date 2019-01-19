900518c1fa912c657dabc7078dfb77a782dd760f
yethuaungptu <yethuaung481995@gamil.com>
2019-01-19 23:24:11 +0630

    add users/teacher init page

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 app.use('/admin/students', student);
 app.use('/admin/teachers', teacher);
 app.use('/admin/subjects', subject);
--- routes/users/teacher.js
+++ routes/users/teacher.js
@@ -6,4 +6,16 @@ router.get('/home', function(req, res, next) {
   res.render('user/teacher/home', { title: 'Express' });
 });
 
+router.get('/timeTab', function(req, res, next) {
+  res.render('user/teacher/time-table', { title: 'Express' });
+});
+
+router.get('/callAtt', function(req, res, next) {
+  res.render('user/teacher/call-att', { title: 'Express' });
+});
+
+router.get('/manageAtt', function(req, res, next) {
+  res.render('user/teacher/manage-att', { title: 'Express' });
+});
+
 module.exports = router;
